edb145ee498a4b573cfa3321030aca4d296c25b8
harun toker <haruntoker@icloud.com>
2024-11-12 20:10:05 +0200

    erorr fixed.

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,11 +2,12 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import getAuthStatus from "./actions";
+
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import getAuthStatus from "./actions";
 
-export default function Page() {
+const Page = () => {
   const [configId, setConfigId] = useState<string | null>(null);
   const router = useRouter();
 
@@ -21,6 +22,7 @@ export default function Page() {
     retry: true,
     retryDelay: 500,
   });
+
   if (data?.success) {
     if (configId) {
       localStorage.removeItem("configurationId");
@@ -35,8 +37,10 @@ export default function Page() {
       <div className="flex flex-col items-center gap-2">
         <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
         <h3 className="font-semibold text-xl">Logging you in...</h3>
-        <p className="">You'll be directed automatically.</p>
+        <p>You will be redirected automatically.</p>
       </div>
     </div>
   );
-}
+};
+
+export default Page;
